75b15afaff9f3d23dcbd0c099fbd123bb1db1c72
1Michael23 <33250781+1Michael23@users.noreply.github.com>
2026-04-29 13:30:47 +1000

    remove old dead code

--- tests/test_env.py
+++
@@ -1,8 +0,0 @@
-def test_core_imports():
-    import numpy as np
-    import pandas as pd
-    import tensorflow as tf
-    from transformers import BertTokenizer
-
-    assert np.__version__.startswith("1.")
-    assert tf.__version__.startswith("2.15")
